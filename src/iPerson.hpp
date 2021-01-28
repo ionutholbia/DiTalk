e74eb3584d5ebd93286d8609f0f6792cd77dd1ed
Ionut Holbia <ionut.holbia@nexthink.com>
2021-01-28 16:27:02 +0100

    Added persons setters to interface.

--- src/iPerson.hpp
+++ src/iPerson.hpp
@@ -6,10 +6,14 @@ class IPerson
 {
 public:
 	virtual std::string getFirstName() = 0;
+	virtual void setFirstName(const std::string& firstName) = 0;
 
 	virtual std::string getLastName() = 0;
+	virtual void setLastName(const std::string& lastName) = 0;
 
 	virtual std::string getPhoneNumber() = 0;
+	virtual void setPhoneNumber(const std::string& phoneNumber) = 0;
 
 	virtual std::string getEmailAddress() = 0;
+	virtual void setEmailAddress(const std::string& emailAddress) = 0;
 };
