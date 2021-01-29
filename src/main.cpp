abb4269d5ac8ad3cb91721e985d53ded85fa0b79
Ionut Holbia <ionut.holbia@nexthink.com>
2021-01-29 12:24:30 +0100

    Working boost di

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
-#include "factory.hpp"
+#include "chore.hpp"
+#include "logger.hpp"
+#include "emailer.hpp"
+#include "texter.hpp"
+#include "person.hpp"
+
 #include <iostream>
+
+#include <boost/di.hpp>
+namespace di = boost::di;
 
 int main(void)
 {
-	auto owner = Factory::createPerson();
+	auto injector = di::make_injector(
+		di::bind<IPerson>.to<Person>(),
+		di::bind<IChore>.to<Chore>(),
+		di::bind<ILogger>.in(di::singleton).to<Logger>(),
+		di::bind<IMessageSender>.in(di::singleton).to<Texter>()
+	);
+
+	auto owner = injector.create<std::shared_ptr<IPerson>>();
 	owner->setFirstName("Ionut");
 	owner->setLastName("Holbia");
 	owner->setEmailAddress("iholbia@ditalk.com");
 	owner->setPhoneNumber("77770000");
-
-	auto chore = Factory::createChore();
-	chore->setChoreName("Read some books.");
+	
+	auto chore = injector.create<std::shared_ptr<IChore>>();
 	chore->setOwner(owner);
+	chore->setChoreName("Read some books.");
 
 	chore->performedWork(3);
 	chore->performedWork(1.5);
